2480910fa2e0bc2261d2e6ea4aef10b263f7c220
bur19149 <56315119+bur19149@users.noreply.github.com>
2020-05-27 04:11:40 +0200

    lösche unnötige Kommentare

--- bin/main.dart
+++ bin/main.dart
@@ -3,6 +3,3 @@ import 'package:http_request/http_request.dart' as http_request;
 main(List<String> arguments) {
   print('Hello world: ${http_request.calculate()}!');
 }
-
-// teste ob es wirklich funktioniert
-// ich bin der pedo bär 8=====================================>
