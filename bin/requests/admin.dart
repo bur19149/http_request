7adae6302189c6e3fddbf9399a7bcb65b5673e51
mrx123 <mrx12335@users.noreply.github.com>
2020-06-03 15:56:42 +0200

    todo hinzugefügt und formatierung geändert

--- bin/requests/admin.dart
+++ bin/requests/admin.dart
@@ -21,7 +21,7 @@ Future<List<objects.Zyklus>> requestZyklen() async {
 }
 
 abstract class User {
-
+//TODO funktioniert nicht
 //  static void erstelleUser(
 //      String vorname, String nachname, int berechtigung, String plz, String ort,
 //      [String jugendgruppe, int elternid, String elternemail, String email]) async {
@@ -44,8 +44,6 @@ abstract class User {
 //          'Unvorhergesehene HTTP-Rückmeldung: ${_response.statusCode}');
 //    }
 //  }
-
-
 
   static Future<objects.User> requestUser(int userID) async {
     var _response = await http.get(
@@ -105,42 +103,42 @@ abstract class Termin {
 
   static loescheUser(int id) async{
     var _response = await http.delete('${variables.url}/admin/user?token=${variables.token}&userid=${id}');
-    if(_response.statusCode==404){
-      throw Exception('Der User Existiert nicht.');
-    }else if(_response.statusCode!=204){
-      throw Exception('Unvorhergesehene HTTP-Rückmeldung: ${_response.statusCode}.');
-    }
-  }
-
-  // @formatter:off
-  static bearbeiteUser(String token, int userID,
-      [String vorname, String nachname, String email, String plz, String ort, String jugendgruppe,
-       int berechtigung, int elternID, String elternmail]) async{
-
-    var parameterListe = <String, dynamic>{};
-                            parameterListe['token']        = token;
-                            parameterListe['userid']       = userID;
-    if(vorname!=null)       parameterListe['vorname']      = vorname;
-    if(nachname!=null)      parameterListe['nachname']     = nachname;
-    if(email!=null)         parameterListe['email']        = email;
-    if(plz!=null)           parameterListe['plz']          = plz;
-    if(ort!=null)           parameterListe['ort']          = ort;
-    if(jugendgruppe!=null)  parameterListe['jugendgruppe'] = jugendgruppe;
-    if(berechtigung!= null) parameterListe['berechtigung'] = berechtigung;
-    if(elternID!=null)      parameterListe['elternID']     = elternID;
-    if(elternmail!=null)    parameterListe['elternmail']   = elternmail;
-
-    var _response = await http.patch(variables.url, body: parameterListe);
     if(_response.statusCode!=204){
       if(_response.statusCode==404){
         throw Exception('Der User Existiert nicht.');
       }else{
         throw Exception('Unvorhergesehene HTTP-Rückmeldung: ${_response.statusCode}.');
       }
-    }// @formatter:on
+    }
   }
 
+  //TODO for GUI PGM'er pfuefung ob mindestens ein optionaler Parameter dabei sind
+  // @formatter:off
+  static bearbeiteUser(String token, int userID,
+      [String vorname, String nachname, String email, String plz, String ort, String jugendgruppe,
+       int berechtigung, int elternID, String elternmail]) async{
 
+    var parameter = <String, dynamic>{};
+                            parameter['token']        = token;
+                            parameter['userid']       = userID;
+    if(vorname!=null)       parameter['vorname']      = vorname;
+    if(nachname!=null)      parameter['nachname']     = nachname;
+    if(email!=null)         parameter['email']        = email;
+    if(plz!=null)           parameter['plz']          = plz;
+    if(ort!=null)           parameter['ort']          = ort;
+    if(jugendgruppe!=null)  parameter['jugendgruppe'] = jugendgruppe;
+    if(berechtigung!= null) parameter['berechtigung'] = berechtigung;
+    if(elternID!=null)      parameter['elternID']     = elternID;
+    if(elternmail!=null)    parameter['elternmail']   = elternmail;
 
-
+    var _response = await http.patch(variables.url, body: parameter);
+    if(_response.statusCode!=204){
+      if(_response.statusCode==404){
+        throw Exception('Der User Existiert nicht.');
+      }else{
+        throw Exception('Unvorhergesehene HTTP-Rückmeldung: ${_response.statusCode}.');
+      }
+    }
+  }
+  // @formatter:on
 }
